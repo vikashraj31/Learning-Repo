94d848d43ed0fe1d7eca22ff14e4b29b4867dfce
Vikash Raj <vikashh3121@gmail.com>
2025-12-24 21:33:45 +0530

    Java Code

---
+++ Java programs/Array/array.java
@@ -0,0 +1,10 @@
+package Array;
+
+class Array {
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+        numbers[0] = 21;
+        numbers[2] = 31;
+        System.out.println("The Value is " + numbers[0]);
+    }
+}
