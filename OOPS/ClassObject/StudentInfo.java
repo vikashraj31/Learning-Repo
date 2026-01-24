0c41c0e701613d7f619f90c027aa4e744605007f
vikashvanshi <vikashh3121@gmail.com>
2026-01-24 14:12:59 +0530

    Use of final key word

--- OOPS/ClassObject/StudentInfo.java
+++ OOPS/ClassObject/StudentInfo.java
@@ -1,13 +1,12 @@
 package OOPS.ClassObject;
 
-// This is Student class
 class Student {
     int age;
     String name;
     String section;
 
-    final String School = "DAV";   // fixed for all students
-    final String DN = "Vikash";    // cannot be changed
+    final String School = "DAV";   // fixed for all students (cannot be changed)
+    final String DN = "Vikash";    
 }
 
 public class StudentInfo {
@@ -17,8 +16,7 @@ public class StudentInfo {
         s1.age = 21;
         s1.name = "Ram";
 
-        // s1.School = "SAV";  //  cannot change final variable
-        // s1.DN = "Rahul";    //  cannot change final variable
+        //s1.School = "SAV";  final things can not be change from main function
 
         System.out.println("Age is " + s1.age +
                            ", Name is " + s1.name +
