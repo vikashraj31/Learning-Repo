fcd03b7923f052e7c5bbdf305984e13753c3c2f1
vikashvanshi <vikashh3121@gmail.com>
2026-01-27 23:59:03 +0530

    Final code of Multi Interface

--- OOPS/Interface/MultiTest.java
+++ OOPS/Interface/MultiTest.java
@@ -2,24 +2,28 @@ package OOPS.Interface;
 
 interface Printable {
     void print();
-
 }
 
-interface Scannable{
+interface Scannable {
     void scan();
 }
 
 class OfficeMachine implements Printable, Scannable {
+
     public void print() {
         System.out.println("This is Print Function");
-        
     }
 
     public void scan() {
-        System.out.println("This is Print Scan");
+        System.out.println("This is Scan Function");
     }
 }
 
 public class MultiTest {
-    
+    public static void main(String[] args) {
+
+        OfficeMachine om = new OfficeMachine();
+        om.print();
+        om.scan();
+    }
 }
