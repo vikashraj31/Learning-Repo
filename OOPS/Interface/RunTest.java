14fbc6eda69c2370a9070f75f95e4cc0a9a0ebd6
vikashvanshi <vikashh3121@gmail.com>
2026-01-27 23:48:48 +0530

    Line in interface Code

--- OOPS/Interface/RunTest.java
+++ OOPS/Interface/RunTest.java
@@ -1,31 +1,29 @@
 package OOPS.Interface;
 
+// This is interface, it hides implementation and provides full abstraction
 interface Machine {
     void start();
 }
 
 class Computer implements Machine {
     public void start() {
-        
+        System.out.println("Computer is starting");
     }
 }
 
 class WashingMachine implements Machine {
     public void start() {
-        
+        System.out.println("Washing Machine is starting");
     }
 }
 
 public class RunTest {
     public static void main(String[] args) {
-        
-    }
-}
 
+        Machine m1 = new Computer();
+        m1.start();
 
-public class RunTest {
-    public static void main (String args []){
-        
+        Machine m2 = new WashingMachine();
+        m2.start();
     }
-    
 }
