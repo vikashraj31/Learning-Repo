5786bc5eaab8dec314671e646a3d4ac34679bad4
vikashvanshi <vikashh3121@gmail.com>
2026-01-27 23:45:18 +0530

    Interface Code

---
+++ OOPS/Interface/RunTest.java
@@ -0,0 +1,31 @@
+package OOPS.Interface;
+
+interface Machine {
+    void start();
+}
+
+class Computer implements Machine {
+    public void start() {
+        
+    }
+}
+
+class WashingMachine implements Machine {
+    public void start() {
+        
+    }
+}
+
+public class RunTest {
+    public static void main(String[] args) {
+        
+    }
+}
+
+
+public class RunTest {
+    public static void main (String args []){
+        
+    }
+    
+}
