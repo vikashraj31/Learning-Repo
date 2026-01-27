f68f25b34ea664a8a9d361996003d9f2b53baeea
vikashvanshi <vikashh3121@gmail.com>
2026-01-28 00:11:04 +0530

    Static method calling using interface name

--- OOPS/Interface/AdvancedTest.java
+++ OOPS/Interface/AdvancedTest.java
@@ -1,27 +1,32 @@
 package OOPS.Interface;
 
 interface Device {
+
     void turnOn();
 
     default void showType() {
-        
+        System.out.println("This is SmartPhone");
     }
 
     static void info() {
-        
+        System.out.println("Device interface information");
     }
 }
 
 class Mobile implements Device {
 
     public void turnOn() {
-        
+        System.out.println("Phone is turned ON");
     }
 }
 
 public class AdvancedTest {
     public static void main(String[] args) {
 
-        
+        Mobile m1 = new Mobile();
+        m1.turnOn();
+        m1.showType();   // default method from interface
+
+        Device.info();  // static method called using interface name
     }
 }
